4b6e7cbc00a1598f289c62026e993b2c987f7445
Mauricio <formal_phoenix2000@hotmail.com>
2025-07-14 17:04:49 -0500

    Cambios V1.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
-  return <div className="App">Portfolio</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Body>Portfolio</Body>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#1C1C27",
+  bgLight: "#1C1E27",
+  primary: "#854CE6",
+  text_primary: "#F2F3F4",
+  text_secondary: "#B1B2B3",
+  card: "#171721",
+  cardLight: "#191924",
+  button: "#854CE6",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#F0F0F0",
+  primary: "#BE1ADB",
+  text_primary: "#111111",
+  text_secondary: "#48494A",
+  card: "#FFFFFF",
+  button: "5C5B5B",
+};
