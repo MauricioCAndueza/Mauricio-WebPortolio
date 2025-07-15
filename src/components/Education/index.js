3efef399f5bd3406291199e2aadc895f903b4b6c
Mauricio <formal_phoenix2000@hotmail.com>
2025-07-14 20:03:31 -0500

    Cambios V1.1

---
+++ src/components/Education/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Education = () => {
+  return <div>Education Section</div>;
+};
+
+export default Education;
--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -3,9 +3,9 @@ export const darkTheme = {
   bgLight: "#1C1E27",
   primary: "#854CE6",
   text_primary: "#F2F3F4",
-  text_secondary: "#B1B2B3",
+  text_secondary: "#b1b2b3",
   card: "#171721",
-  cardLight: "#191924",
+  card_light: "#191924",
   button: "#854CE6",
   white: "#FFFFFF",
   black: "#000000",
@@ -13,10 +13,10 @@ export const darkTheme = {
 
 export const lightTheme = {
   bg: "#FFFFFF",
-  bgLight: "#F0F0F0",
-  primary: "#BE1ADB",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
   text_primary: "#111111",
-  text_secondary: "#48494A",
+  text_secondary: "#48494a",
   card: "#FFFFFF",
-  button: "5C5B5B",
+  button: "#5c5b5b",
 };
